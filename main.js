b6f5efd6d47bc3a903f62ef07a900496ea7da2a3
Arlen <nasirov.munar1976@gmail.com>
2026-06-19 16:13:00 +0600

    final result

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ for (let e = 1; e <= 10; e++) {
 
 // 6
 
-for (let f = 1; f <= 11; f++) {
+for (let f = 1; f <= 10; f++) {
     console.log(f * 5);
 }
 
@@ -64,7 +64,7 @@ for (let h = 0; h <= 100; h += 5) {
 
 let sum2 = 0;
 
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
     if (i % 7 === 0) {
         sum2++;
     }
@@ -114,4 +114,178 @@ for (let key in user) {
 
 // 7
 
-console.log(Object.values(user));
+for (let key in user) {
+    console.log(user[key]);
+}
+
+// 8
+
+console.log(Object.keys(user).length);
+
+
+// Массивы
+
+// 1
+
+const numbers = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+
+
+// 2
+
+let sum3 = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum3 += numbers[i];
+}
+
+console.log(sum3);
+
+
+// 3
+
+let max = numbers[0];
+
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+        max = numbers[i];
+    }
+}
+
+console.log(max);
+
+
+// 4
+
+let min = numbers[0];
+
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] < min) {
+        min = numbers[i];
+    }
+}
+
+console.log(min);
+
+
+// 5
+
+console.log(numbers.length);
+
+
+// 6
+
+let evenSum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        evenSum += numbers[i];
+    }
+}
+
+console.log(evenSum);
+
+
+// 7
+
+let negativeCount = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 0) {
+        negativeCount++;
+    }
+}
+
+console.log(negativeCount);
+
+
+// 8
+
+let product = 1;
+
+for (let i = 0; i < numbers.length; i++) {
+    product *= numbers[i];
+}
+
+console.log(product);
+
+
+// 9
+
+let maxIndex = 0;
+
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > numbers[maxIndex]) {
+        maxIndex = i;
+    }
+}
+
+console.log(maxIndex);
+
+
+// 10
+
+let minIndex = 0;
+
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] < numbers[minIndex]) {
+        minIndex = i;
+    }
+}
+
+console.log(minIndex);
+
+
+// 11
+
+let found = false;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === 10) {
+        found = true;
+    }
+}
+
+console.log(found);
+
+
+// 12
+
+let sevenCount = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === 7) {
+        sevenCount++;
+    }
+}
+
+console.log(sevenCount);
+
+
+// 13
+
+let lastPositive;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        lastPositive = numbers[i];
+    }
+}
+
+console.log(lastPositive);
+
+
+// 14
+
+let total = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+}
+
+let average = total / numbers.length;
+
+console.log(average);
